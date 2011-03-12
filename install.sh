95da7eea897a363f4182c58bd7dc43166130af78
Daniel Jomphe <danieljomphe@gmail.com>
2011-03-11 23:44:17 -0500

    add install scripts shamelessly copied from stuartsierra

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd `dirname $0`
+export DOTFILES=`pwd`
+
+source $DOTFILES/install_functions.sh
+
+#update_submodules
+
+link_with_backup .bashrc
+link_with_backup .bash_profile
+link_with_backup .gitconfig
+
+link_with_backup .emacs.d
+#install_elpa
+#link_with_backup .emacs
+
+#install_relevance_etc
+
+
+#backup ~/.relevance-etc
+#ln -s $DOTFILES/submodules/relevance/etc $HOME/.relevance-etc
