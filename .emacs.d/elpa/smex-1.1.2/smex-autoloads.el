a79d4dd4a794b51face40b59a1b90c0c0e13f890
Daniel Jomphe <danieljomphe@gmail.com>
2011-12-23 14:06:46 -0500

    update packages on new computer

---
+++ .emacs.d/elpa/smex-1.1.2/smex-autoloads.el
@@ -0,0 +1,27 @@
+;;; smex-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (smex-initialize) "smex" "smex.el" (20210 36871))
+;;; Generated autoloads from smex.el
+
+(autoload 'smex-initialize "smex" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("smex-pkg.el") (20210 36871 191610))
+
+;;;***
+
+(provide 'smex-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; smex-autoloads.el ends here
