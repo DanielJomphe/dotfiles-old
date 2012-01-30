4338c9fa89006744915ee30bf523869ef185efb7
Daniel Jomphe <danieljomphe@gmail.com>
2012-01-30 17:23:11 -0500

    Add & update packages.

--- .emacs.d/elpa/magit-1.0.0/magit-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; magit-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (magit-status) "magit" "magit.el" (19927 60496))
-;;; Generated autoloads from magit.el
-
-(autoload 'magit-status "magit" "\
-
-
-\(fn DIR)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("50magit.el" "magit-key-mode.el" "magit-pkg.el"
-;;;;;;  "magit-svn.el" "magit-topgit.el") (19927 60497 13523))
-
-;;;***
-
-(provide 'magit-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; magit-autoloads.el ends here
