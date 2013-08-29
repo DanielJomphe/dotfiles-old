5ce0d404109a6aa29fd2b69299766fb8bfb16a07
Daniel Jomphe <danieljomphe@gmail.com>
2013-08-29 11:36:24 -0400

    Improve git config.
    - Move git config into ~/.config/git/*.
    - Set basic commit template for 54/72 summary and desc.
    - Update global ignores.
    - Update ignores for this repo.
    - Allow sourcetree to be used as a diff/merge tool.

--- install.sh
+++ install.sh
@@ -11,10 +11,11 @@ link_with_backup .bashrc
 link_with_backup .bash_profile
 link_with_backup .profile
 link_with_backup .mypath
-link_with_backup .gitconfig
-link_with_backup .gitignore_global
-link_with_backup .git_commit_template
+link_with_backup .lein
 
 link_with_backup  .emacs-live.el
-link_with_backup_2 emacs-live       .emacs.d
-link_with_backup_2 emacs-live-packs .emacs.d.packs
+link_with_backup_2 emacs-live           .emacs.d
+link_with_backup_2 emacs-live-packs     .emacs.d.packs
+link_with_backup_2 .gitconfig           .config/git/config
+link_with_backup_2 .gitignore_global    .config/git/ignore
+link_with_backup_2 .git_commit_template .config/git/git_commit_template
